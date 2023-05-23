a72ef86c9127e86be8779e98a0ddffb7ceec8757
netasananes <netasananes@gmail.com>
2023-05-23 17:03:15 +0300

    hi

--- main.c
+++ main.c
@@ -4,98 +4,91 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define MAX_LINE_LENGTH 100
+#define MAX_LINE_LENGTH 150
 int typeCount [3]={0};
-
 int prodNum = 1;
-
-typedef struct Node {
-    char ** data ;
-    struct Node* next;
-} Node;
-
 
 typedef struct BoundedQueue {
     int size;
-    char ** data ;
+    int start;
+    int end;
+    char **articles;
     pthread_mutex_t mutex;
     sem_t empty;
     sem_t full;
-    int start;
-    int end;
-
 } BoundedQueue;
 
-BoundedQueue* prodArr;
 typedef struct Producer{
-    int numOfProducts;
+    int numOfArticles;
     int prodNum;
     int size;
     BoundedQueue bq;
 }Producer;
-typedef struct UnboundedQueue {
-    Node* prev;
-    Node* next;
-} UnboundedQueue;
+
+BoundedQueue * prodArr;
 
 typedef struct CreationArg {
     int numOfProd;
-    int numOfProducts;
+    int numOfArticles;
     int size;
 } CreationArg;
 
 
 BoundedQueue createBoundedQueue(int size){
-    BoundedQueue bq;
-    bq.data= malloc(size * sizeof(char*));
-    pthread_mutex_init(&bq.mutex,NULL);
-    sem_init(&bq.empty, 0, size);
-    sem_init(&bq.full, 0, 0);
-    bq.start=size;
-    bq.end=size;
-    return bq;
+    BoundedQueue* bq= malloc(sizeof (BoundedQueue));
+    bq->articles= (char**)malloc(size * sizeof(char*));
+    pthread_mutex_init(&bq->mutex,NULL);
+    sem_init(&bq->empty, 0, size);
+    sem_init(&bq->full, size, 0);/// 0?
+    bq->start=size-1;
+    bq->end=size-1;
+    bq->size=size;
+    return *bq;
 }
+
 void insertBoundedQueue(char * article, int prodNumber){
     BoundedQueue bq=prodArr[prodNumber];
     sem_wait(&bq.empty);
     pthread_mutex_lock(&bq.mutex);
-    strcpy(bq.data[bq.start],article);
+    bq.articles[bq.end] = article;
+    bq.end = (bq.end + 1) % bq.size;
     pthread_mutex_unlock(&bq.mutex);
     sem_post(&bq.full);
-    bq.start--;
-
 }
-void removeBoundedQueue(int prodNumber){
+
+char *removeBoundedQueue(int prodNumber){
     BoundedQueue bq=prodArr[prodNumber];
     sem_wait(&bq.empty);
     pthread_mutex_lock(&bq.mutex);
-    strcpy(bq.data[bq.end],NULL);
+    char* removedArticle = bq.articles[bq.start];
+    bq.start = (bq.start - 1) % bq.size;
     pthread_mutex_unlock(&bq.mutex);
     sem_post(&bq.full);
-    bq.end--;
+    return removedArticle;
 }
 
 void createArticles(void *arg){
     Producer* prod= (Producer*)arg;
-    char * article;
+    char article[MAX_LINE_LENGTH];
     int i,type ;
-    for (i=0;i<=prod->numOfProducts;i++){
-        if(i==prod->numOfProducts){
+    for (i=0;i<=prod->numOfArticles; i++){
+        if(i==prod->numOfArticles){
             sprintf(article,"Done");
         }
         type= rand() % 3 + 1;
         if(type==1){
-            sprintf(article,("producer %d %s %d\n",prod->prodNum,"SPORTS",typeCount[0]));
+            sprintf(article, "producer %d %s %d\n", prod->prodNum, "SPORTS", typeCount[0]);
             typeCount[0]++;
         } else if (type==2){
-            sprintf(article,("producer %d %s %d\n",prod->prodNum,"NEWS",typeCount[1]));
+            sprintf(article, "producer %d %s %d\n", prod->prodNum, "NEWS", typeCount[1]);
+
             typeCount[1]++;
         } else if (type==3){
-            sprintf(article,("producer %d %s %d\n",prod->prodNum,"WEATHER",typeCount[2]));
+            sprintf(article, "producer %d %s %d\n", prod->prodNum, "WEATHER", typeCount[2]);
             typeCount[2]++;
         }
     }
-    char* articleToInsert= malloc(strlen(article)*sizeof (char));
+    char* articleToInsert= malloc(strlen(article) * sizeof(char));
     strcpy(articleToInsert,article);
     insertBoundedQueue(articleToInsert,prod->prodNum);
 }
@@ -105,7 +98,7 @@ void createProducer(void *argPtr) {
     CreationArg* arg = (CreationArg*)argPtr;
     Producer prod;
     prod.prodNum=arg->numOfProd;
-    prod.numOfProducts=arg->numOfProducts;
+    prod.numOfArticles=arg->numOfArticles;
     prod.size=arg->size;
     prod.bq= createBoundedQueue(arg->size);
     prodArr[arg->numOfProd]=prod.bq;
@@ -113,12 +106,23 @@ void createProducer(void *argPtr) {
 
 }
 
+void createDispatcher(){
+    char* article;
+    int *flags = calloc(prodNum, sizeof(int));
+    int i;
+    for(i=0;i<prodNum;i++) {
+        if (!flags[i]) {
+            article = removeBoundedQueue(i);
+            printf("%s\n", article);
+        }
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         perror("configuration file is missing\n");
         return -1;
     }
-
     const char* file_name = argv[1];
     FILE* file = fopen(file_name, "r");
     if (file == NULL) {
@@ -134,15 +138,11 @@ int main(int argc, char* argv[]) {
             if (fgets(qSize, MAX_LINE_LENGTH, file) != NULL) {
                 pthread_t threadId;
                 CreationArg* arg = malloc(sizeof(CreationArg));
-                if (arg == NULL) {
-                    perror("Failed to allocate memory for CreationArg\n");
-                    return 1;
-                }
                 if(prodNum!=1) {
                     prodArr = realloc(prodArr, 1 * sizeof(BoundedQueue));
                 }
                 arg->numOfProd = atoi(line);
-                arg->numOfProducts = atoi(numOfProducts);
+                arg->numOfArticles = atoi(numOfProducts);
                 arg->size = atoi(qSize);
 
                 returnValue = pthread_create(&threadId, NULL, createProducer, (void*)arg);
@@ -164,17 +164,15 @@ int main(int argc, char* argv[]) {
             perror("configuration file is not in the required format\n");
         }
     }
-    int i;
-    pthread_t threadId2;
-    for(i=0;i<prodNum;i++){
-   //     returnValue = pthread_create(&threadId2, NULL, createArticles,&prodArr[i]);
 
-        if (returnValue != 0) {
-            perror("Error creating thread\n");
-            return -1;
-        }
+  //  fclose(file);
+    pthread_t threadId2;
+    returnValue = pthread_create(&threadId2, NULL, createDispatcher, NULL);
+
+    if (returnValue != 0) {
+        perror("Error creating thread\n");
+        return -1;
     }
-    fclose(file);
 
     return 0;
 }
